56112935a9170c218e4e59b0c9d80370ebd4a40a
ultinomics <dn95@georgetown.edu>
2014-03-21 03:58:01 -0400

    updated run_directory

--- ( ) _run_zillow_directory.bat
+++ ( ) _run_zillow_directory.bat
@@ -6,15 +6,11 @@ REM CANNOT HAVE SPACES AFTER VARIABLE NAMES
 REM "STATA =" and "STATA=" are DIFFERENT!
 set STATA=D:\Program Files (x86)\Stata13\
 set GIT=D:\Dan's Workspace\GitHub Repository\zillow_projects
-set STATA
-set GIT
 
-REM "%STATA%\StataMP-64" do "%GIT%\(0) initialize_zillow.do"
-REM "%STATA%\StataMP-64" do "%GIT%\(1) zillow_trimdown.do"
-REM "%STATA%\StataMP-64" do "%GIT%\(2) zillow_76_to_text.do"
+ "%STATA%\StataMP-64" do "%GIT%\(0) initialize_zillow.do"
+ "%STATA%\StataMP-64" do "%GIT%\(1) zillow_trimdown.do"
+ "%STATA%\StataMP-64" do "%GIT%\(2) zillow_76_to_text.do"
 python "%GIT%\(3.1) zillow_tokenize_atype76.py"
 python "%GIT%\(3.2) tokenize_greenhomes.py"
-REM "%STATA%\StataMP-64" do "%GIT%\(4) zillow_wordlist.do"
-REM "%STATA%\StataMP-64" "%GIT%\dict_wordlist_merge.do"
 
 
